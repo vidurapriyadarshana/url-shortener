248bbaed957a296423f87dfaee09d65785e57406
Vidura Priyadarshana <vidura200427@gmail.com>
2025-04-02 19:09:37 +0530

    Header/Footer complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="container">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p className="col-md-4 mb-0 text-body-secondary">© 2025 Vidura, Inc</p>
+        <ul className="nav col-md-4 justify-content-end">
+          
+          <li className="nav-item">
+            <NavLink to='/' className='nav-link'>
+              Home
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink to='/about' className='nav-link'>
+              About
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <a href="https://github.com/vidurapriyadarshana/url-shortener.git" className='nav-link'>
+              GitHub
+            </a>
+          </li>
+          
+        </ul>
+      </footer>
+    </div>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import AboutPage from '../pages/AboutPage'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="container">
+      <header className="d-flex justify-content-center py-3">
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <NavLink to='/' className='nav-link'>
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to='/about' className='nav-link'>
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </header>
+    </div>
   )
 }
 
